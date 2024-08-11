bfc9a45c30cb2eb9005d62b7852c6c073d59210f
Waghib <Waghibahmad30@gmail.com>
2024-08-11 23:57:44 +0500

    connecting mongodb with nodejs

--- index.js
+++ index.js
@@ -1,40 +1,15 @@
-const { count } = require('console');
-const express = require('express');
-const path = require('path');
+const {MongoClient} = require('mongodb');
+const url = 'mongodb://localhost:27017';
+const databaseName = 'e-comm';
+const client = new MongoClient(url);
 
-const app = express();
+async function getData()
+{
+    let result = await client.connect();
+    db = client.db(databaseName);
+    collection = db.collection('products');
+    let data = await collection.find({}).toArray();
+    console.log(data);
+}
 
-const publicPath = path.join(__dirname, 'public');
-
-app.set('view engine', 'ejs');
-
-app.get('/profile', (req, res) => {
-    const user = {
-        name: 'John Doe',
-        age: 30,
-        email: 'abc@test.com',
-        country: 'USA',   
-        skills: ['HTML', 'CSS', 'JavaScript', 'Node', 'Express', 'MongoDB'],
-    }
-    res.render('profile', {user});
-});
-
-app.get('/login', (req, res) => {
-    res.render('login');
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(`${publicPath}/index.html`);
-});
-
-app.get('/about', (req, res) => {
-    res.sendFile(`${publicPath}/about.html`);
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(`${publicPath}/404.html`);
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+getData();
