9948f4358064a4536ec88f223172e2e3120c6773
Waghib <Waghibahmad30@gmail.com>
2024-08-15 22:18:37 +0500

    POST API with mongodb

--- api.js
+++ api.js
@@ -2,12 +2,21 @@ const dbConnect = require('./mongodb');
 const express = require('express');
 const app = express();
 
+// middleware to parse the request body
+app.use(express.json());
 
-app.get('/mobiles', async (req, res) => {
+app.get('/', async (req, res) => {
     const data = await dbConnect();
     const result = await data.find({}).toArray();
     res.send(result);
 
+});
+
+app.post('/', async (req, res) => {
+    console.log(req.body);
+    const data = await dbConnect();
+    const result = await data.insertOne(req.body);
+    res.send(result);
 });
 
 app.listen(3000)
