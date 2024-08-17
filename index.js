604d1d762283e349ecaea25ba24f4aa82944def0
Waghib <Waghibahmad30@gmail.com>
2024-08-17 23:04:39 +0500

    GET, DELETE, and PUT api method with mongoose

--- index.js
+++ index.js
@@ -14,4 +14,19 @@ app.post('/create', async (req, res) => {
 
 });
 
+app.get('/list', async (req, res) => {
+  const result = await product.find();
+  res.send(result);
+});
+
+app.delete('/delete/:_id', async (req, res) => {
+  const result = await product.findByIdAndDelete(req.params._id);
+  res.send(result);
+});
+
+app.put('/update/:_id', async (req, res) => {
+  const result = await product.findByIdAndUpdate(req.params._id, req.body);
+  res.send(result);
+});
+
 app.listen(3000);
