b404c48b9d999064eaeea2d4d2999a46843b44be
Waghib <Waghibahmad30@gmail.com>
2024-08-17 23:18:58 +0500

    search API with multiple fields

--- index.js
+++ index.js
@@ -6,27 +6,41 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/create', async (req, res) => {
-  let data = new product(req.body);
-  const result = await data.save();
-  console.log(req.body);
-  res.send(result);
+// app.post('/create', async (req, res) => {
+//   let data = new product(req.body);
+//   const result = await data.save();
+//   console.log(req.body);
+//   res.send(result);
+// });
 
+// app.get('/list', async (req, res) => {
+//   const result = await product.find();
+//   res.send(result);
+// });
+
+// app.delete('/delete/:_id', async (req, res) => {
+//   const result = await product.findByIdAndDelete(req.params._id);
+//   res.send(result);
+// });
+
+// app.put('/update/:_id', async (req, res) => {
+//   const result = await product.findByIdAndUpdate(req.params._id, req.body);
+//   res.send(result);
+// });
+
+
+app.get('/search/:key', async (req, res) => {
+  const result = await product.find({ 
+    $or: [
+      {name: { $regex: req.params.key} },
+      // {brand: req.params.key},
+      // {category: req.params.key}
+    ]
+   });
+  // console.log(result);
+  res.send(result);
 });
 
-app.get('/list', async (req, res) => {
-  const result = await product.find();
-  res.send(result);
+app.listen(3000, () => {
+  console.log('Server started');
 });
-
-app.delete('/delete/:_id', async (req, res) => {
-  const result = await product.findByIdAndDelete(req.params._id);
-  res.send(result);
-});
-
-app.put('/update/:_id', async (req, res) => {
-  const result = await product.findByIdAndUpdate(req.params._id, req.body);
-  res.send(result);
-});
-
-app.listen(3000);
